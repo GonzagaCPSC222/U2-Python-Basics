3d71185b4d8fe847ec941f97a0a80950c4c71b2d
Gina Sprint <sprint@gonzaga.edu>
2022-09-08 13:32:40 -0700

    start of conditionals

--- PythonBasicsFun/main.py
+++ PythonBasicsFun/main.py
@@ -1,3 +1,5 @@
+import math
+
 # this is a one line commentgjfakhdfklghadkfjhg
 # a comment is used to document your code
 # comments are ignored by python
@@ -51,3 +53,41 @@ print(5 // 2)
 print(5 % 2)
 # ** is the exponentiation operator (power)
 print(5 ** 2)
+
+# warm up
+# print(3.0 % 1)
+# print(3 / 0)
+# print(2 ** 4 ** (2 / 4))
+
+# we can also do exponentiation with the pow() function
+# we need to import the math module
+# a module is a python file
+print(math.pow(5, 2))
+
+# GETTING INPUT FROM THE USER
+print("Enter your favorite number: ")
+fav_num = input()
+print(type(fav_num))
+print("Your favorite number doubled is:", 2 * int(fav_num))
+# we often need to convert between types
+# type conversion
+fav_num_int = int(fav_num)
+print("Your favorite number doubled is:", 2 * fav_num_int)
+
+# DECIMAL FORMATTING
+# there are LOTS of ways to do this!!
+print(math.pi)
+print("%.2f" %(math.pi))
+print("{:.2f}".format(math.pi))
+print(f"{math.pi:.2f}")
+print(round(math.pi, 2))
+
+# CONDITIONALS (AKA if statements)
+# if some condition (boolean condition) is true
+# then execute some code
+x = 7
+if x == 5: # == is the equality operator
+    print("hello")
+    # python uses indentation (1 tab = 4 spaces)
+    # to group code together into "blocks"
+    # like { } C/C++
